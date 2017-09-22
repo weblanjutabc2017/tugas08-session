838fd98009e86efc395d3b4ce0c481c5dfa70820
tomyspt <saputratomy325@gmail.com>
2017-09-22 11:55:16 +0700

    mengumpulkan tugas 09

---
+++ praktikum8.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+ -- version 4.4.14
+ -- http://www.phpmyadmin.net
+ --
+ -- Host: 127.0.0.1
+ -- Generation Time: Sep 18, 2017 at 01:22 AM
+ -- Server version: 5.6.26
+ -- PHP Version: 5.6.12
+ 
+ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+ SET time_zone = "+00:00";
+ 
+ 
+ /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+ /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+ /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ /*!40101 SET NAMES utf8mb4 */;
+ 
+ --
+ -- Database: `praktikum8`
+ --
+ 
+ -- --------------------------------------------------------
+ 
+ --
+ -- Table structure for table `user`
+ --
+ 
+ CREATE TABLE IF NOT EXISTS `user` (
+   `id` int(11) NOT NULL,
+   `username` varchar(20) NOT NULL,
+   `password` varchar(20) NOT NULL,
+   `level` varchar(10) NOT NULL
+ ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+ 
+ --
+ -- Dumping data for table `user`
+ --
+ 
+ INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
+ (1, 'salim', 'romadhon', 'admin'),
+ (2, 'hafidz', 'taqiudin', 'user');
+ 
+ --
+ -- Indexes for dumped tables
+ --
+ 
+ --
+ -- Indexes for table `user`
+ --
+ ALTER TABLE `user`
+   ADD PRIMARY KEY (`id`),
+   ADD UNIQUE KEY `username` (`username`);
+ 
+ --
+ -- AUTO_INCREMENT for dumped tables
+ --
+ 
+ --
+ -- AUTO_INCREMENT for table `user`
+ --
+ ALTER TABLE `user`
+   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+ /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+ /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+ /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
